6213d01215eae8606f91b28d513f62f21215b24d
Allison Machado <aallison93@gmail.com>
2024-11-01 12:23:36 +0000

    refactor: improve the move logic code

--- packages/robotic-rovers-api/src/services/robotic-rover.ts
+++ packages/robotic-rovers-api/src/services/robotic-rover.ts
@@ -4,6 +4,139 @@ import { RoboticRover } from '../entities/RoboticRover';
 import { BusinessValidationError } from '../util/validation-error';
 import { logger } from '../util/logger';
 import { MoveInstruction } from '../entities/MoveInstruction';
+import { Repository } from 'typeorm';
+
+const rotate = (orientation: 'N' | 'E' | 'S' | 'W', direction: 'L' | 'R'): 'N' | 'E' | 'S' | 'W' => {
+  const orientations = ['N', 'E', 'S', 'W'];
+
+  const currentIndex = orientations.indexOf(orientation);
+
+  if (direction === 'L') {
+    return orientations[(currentIndex + 3) % 4] as 'N' | 'E' | 'S' | 'W';
+  } else {
+    return orientations[(currentIndex + 1) % 4] as 'N' | 'E' | 'S' | 'W';
+  }
+};
+
+const saveRotation = async ({
+  targetRover,
+  direction,
+  roverRepository,
+  moveInstructionRepository,
+}: {
+  targetRover: RoboticRover;
+  direction: 'L' | 'R';
+  roverRepository: Repository<RoboticRover>;
+  moveInstructionRepository: Repository<MoveInstruction>;
+}) => {
+  targetRover.orientation = rotate(targetRover.orientation, direction);
+
+  await roverRepository.save(targetRover);
+  await moveInstructionRepository.insert({
+    code: direction,
+    xNextPosition: targetRover.xCurrentPosition,
+    yNextPosition: targetRover.yCurrentPosition,
+    nextOrientation: targetRover.orientation,
+    roboticRover: targetRover,
+  });
+
+  return {
+    instruction: direction,
+    position: {
+      x: targetRover.xCurrentPosition,
+      y: targetRover.yCurrentPosition,
+    },
+    orientation: targetRover.orientation,
+  };
+};
+
+const saveMove = async ({
+  targetRover,
+  roverRepository,
+  moveInstructionRepository,
+}: {
+  targetRover: RoboticRover;
+  roverRepository: Repository<RoboticRover>;
+  moveInstructionRepository: Repository<MoveInstruction>;
+}) => {
+  const nextPosition = {
+    x: targetRover.xCurrentPosition,
+    y: targetRover.yCurrentPosition,
+  };
+
+  switch (targetRover.orientation) {
+    case 'N':
+      nextPosition.y += 1;
+      break;
+    case 'E':
+      nextPosition.x += 1;
+      break;
+    case 'S':
+      nextPosition.y -= 1;
+      break;
+    case 'W':
+      nextPosition.x -= 1;
+      break;
+  }
+
+  if (
+    nextPosition.x < 0 ||
+    nextPosition.y < 0 ||
+    nextPosition.x >= targetRover.plateau.xWidth ||
+    nextPosition.y >= targetRover.plateau.yHeight
+  ) {
+    logger.debug({
+      action: 'move',
+      message: 'Rover on next position would be out of bounds',
+      nextPosition,
+      plateau: targetRover.plateau,
+    });
+
+    return null;
+  }
+
+  const rovers = await roverRepository.find({
+    where: {
+      plateau: targetRover.plateau,
+      xCurrentPosition: nextPosition.x,
+      yCurrentPosition: nextPosition.y,
+    },
+    lock: { mode: 'pessimistic_write' },
+  });
+
+  if (rovers.length > 0) {
+    logger.debug({
+      action: 'move',
+      message: 'Rover on next position would collide with another rover',
+      nextPosition,
+      conflictingRovers: rovers,
+      plateau: targetRover.plateau,
+    });
+
+    return null;
+  }
+
+  targetRover.xCurrentPosition = nextPosition.x;
+  targetRover.yCurrentPosition = nextPosition.y;
+
+  await roverRepository.save(targetRover);
+  await moveInstructionRepository.insert({
+    code: 'M',
+    xNextPosition: nextPosition.x,
+    yNextPosition: nextPosition.y,
+    nextOrientation: targetRover.orientation,
+    roboticRover: targetRover,
+  });
+
+  return {
+    instruction: 'M',
+    position: {
+      x: targetRover.xCurrentPosition,
+      y: targetRover.yCurrentPosition,
+    },
+    orientation: targetRover.orientation,
+  };
+};
 
 export const create = async (args: {
   plateauId: number;
@@ -58,18 +191,6 @@ export const create = async (args: {
   });
 };
 
-const rotate = (orientation: 'N' | 'E' | 'S' | 'W', direction: 'L' | 'R'): 'N' | 'E' | 'S' | 'W' => {
-  const orientations = ['N', 'E', 'S', 'W'];
-
-  const currentIndex = orientations.indexOf(orientation);
-
-  if (direction === 'L') {
-    return orientations[(currentIndex + 3) % 4] as 'N' | 'E' | 'S' | 'W';
-  } else {
-    return orientations[(currentIndex + 1) % 4] as 'N' | 'E' | 'S' | 'W';
-  }
-};
-
 export const move = async (args: {
   roverId: number;
   instructions: ('L' | 'R' | 'M')[];
@@ -97,135 +218,48 @@ export const move = async (args: {
       },
     });
 
-    const currentPosition = {
-      x: targetRover.xCurrentPosition,
-      y: targetRover.yCurrentPosition,
-    };
-    let orientation = targetRover.orientation;
-
     const appliedMoves = [];
 
     for (const instruction of args.instructions) {
       switch (instruction) {
         case 'L': {
-          orientation = rotate(orientation, 'L');
-          targetRover.orientation = orientation;
-          await entityManager.withRepository(roverRepository).save(targetRover);
-          await entityManager.withRepository(moveInstructionRepository).insert({
-            code: 'L',
-            xNextPosition: currentPosition.x,
-            yNextPosition: currentPosition.y,
-            nextOrientation: orientation,
-            roboticRover: targetRover,
+          const move = await saveRotation({
+            targetRover,
+            direction: 'L',
+            roverRepository: entityManager.withRepository(roverRepository),
+            moveInstructionRepository: entityManager.withRepository(moveInstructionRepository),
           });
-          appliedMoves.push({
-            instruction: 'L',
-            position: {
-              x: currentPosition.x,
-              y: currentPosition.y,
-            },
-            orientation,
-          });
+
+          appliedMoves.push(move);
           break;
         }
         case 'R': {
-          orientation = rotate(orientation, 'R');
-          targetRover.orientation = orientation;
-          await entityManager.withRepository(roverRepository).save(targetRover);
-          await entityManager.withRepository(moveInstructionRepository).insert({
-            code: 'R',
-            xNextPosition: currentPosition.x,
-            yNextPosition: currentPosition.y,
-            nextOrientation: orientation,
-            roboticRover: targetRover,
+          const move = await saveRotation({
+            targetRover,
+            direction: 'R',
+            roverRepository: entityManager.withRepository(roverRepository),
+            moveInstructionRepository: entityManager.withRepository(moveInstructionRepository),
           });
-          appliedMoves.push({
-            instruction: 'R',
-            position: {
-              x: currentPosition.x,
-              y: currentPosition.y,
-            },
-            orientation,
-          });
+
+          appliedMoves.push(move);
           break;
         }
         case 'M': {
-          switch (orientation) {
-            case 'N':
-              currentPosition.y += 1;
-              break;
-            case 'E':
-              currentPosition.x += 1;
-              break;
-            case 'S':
-              currentPosition.y -= 1;
-              break;
-            case 'W':
-              currentPosition.x -= 1;
-              break;
-          }
+          const move = await saveMove({
+            targetRover,
+            roverRepository: entityManager.withRepository(roverRepository),
+            moveInstructionRepository: entityManager.withRepository(moveInstructionRepository),
+          });
 
-          if (
-            currentPosition.x < 0 ||
-            currentPosition.y < 0 ||
-            currentPosition.x >= targetRover.plateau.xWidth ||
-            currentPosition.y >= targetRover.plateau.yHeight
-          ) {
-            logger.debug({
-              action: 'move',
-              message: 'Rover is out of bounds',
-              currentPosition,
-              plateau: targetRover.plateau,
-            });
-
+          if (!move) {
+            // cannot keep processing moves
             return {
               appliedMoves,
               updatedRover: targetRover,
             };
           }
 
-          const rovers = await entityManager.withRepository(roverRepository).find({
-            where: {
-              plateau: targetRover.plateau,
-              xCurrentPosition: currentPosition.x,
-              yCurrentPosition: currentPosition.y,
-            },
-            lock: { mode: 'pessimistic_write' },
-          });
-
-          if (rovers.length > 0) {
-            logger.debug({
-              action: 'move',
-              message: 'Rover is colliding with another rover',
-              currentPosition,
-              plateau: targetRover.plateau,
-            });
-
-            return {
-              appliedMoves,
-              updatedRover: targetRover,
-            };
-          }
-
-          targetRover.xCurrentPosition = currentPosition.x;
-          targetRover.yCurrentPosition = currentPosition.y;
-          await entityManager.withRepository(roverRepository).save(targetRover);
-          await entityManager.withRepository(moveInstructionRepository).insert({
-            code: 'M',
-            xNextPosition: currentPosition.x,
-            yNextPosition: currentPosition.y,
-            nextOrientation: orientation,
-            roboticRover: targetRover,
-          });
-          appliedMoves.push({
-            instruction: 'M',
-            position: {
-              x: currentPosition.x,
-              y: currentPosition.y,
-            },
-            orientation,
-          });
-          break;
+          appliedMoves.push(move);
         }
       }
     }
